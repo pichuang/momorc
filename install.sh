d52c3ea7889efeebacfa05abafbd034b5c7e94e5
Phil Huang <phil.huang@redhat.com>
2019-11-21 12:44:31 +0800

    Update
    Signed-off-by: Phil Huang <phil.huang@redhat.com>

--- install.sh
+++ install.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 # Install zsh
+yum install zsh -y
+cd $HOME
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install zsh plugins
-./install_zsh_plugins.sh
+./.momorc/install_zsh_plugins.sh
 
 # Make softlink
 cd $HOME
+rm .zshrc
 ln -s $HOME/.momorc/.zshrc .zshrc
+
+rm .vimrc
 ln -s $HOME/.momorc/.vimrc .vimrc
+
+rm .bashrc
 ln -s $HOME/.momorc/.bashrc .bashrc
+
+rm .tmux.conf
 ln -s $HOME/.momorc/.tmux.conf .tmux.conf
+
+rm .gitconfig
 ln -s $HOME/.momorc/.gitconfig .gitconfig
+
+rm .gitignore
 ln -s $HOME/.momorc/.gitignore .gitignore
-ln -s ~/.momorc/pichuang.zsh-theme $HOME/.oh-my-zsh/themes/pichuang.zsh-theme
+
+ln -s $HOME/.momorc/pichuang.zsh-theme $HOME/.oh-my-zsh/themes/pichuang.zsh-theme
 
