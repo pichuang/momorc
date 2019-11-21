625631eaeb1fa15aabf643af3513bf25290aeac2
Phil Huang <phil.huang@redhat.com>
2019-11-21 11:34:42 +0800

    Add install_zsh_plugins.sh
    Signed-off-by: Phil Huang <phil.huang@redhat.com>

--- install_zsh_plugins.sh
+++ install_zsh_plugins.sh
@@ -12,3 +12,9 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+#
+# zsh-completions
+#
+# https://github.com/zsh-users/zsh-completions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
